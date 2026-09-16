b3691522cd4214d86e5b520e3db2c58ff95a0639
IJCC <canalbiblioguia@gmail.com>
2026-09-16 13:54:50 -0300

    Alterando o arquivo 'conexao'

--- python/banco/conexao.py
+++ python/banco/conexao.py
@@ -1,7 +1,6 @@
 import sqlite3
 import os
 
-print(">>> conexao.py foi carregado!")
 
 def conectar():
     caminho = os.path.join(
@@ -9,8 +8,7 @@ def conectar():
         "uber2.db"
     )
 
-    print(">>> BANCO USADO:", caminho)
-
     conexao = sqlite3.connect(caminho)
     conexao.execute("PRAGMA foreign_keys = ON")
+
     return conexao
